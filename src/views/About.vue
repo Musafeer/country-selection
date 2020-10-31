3088990feed52568ba87ac65cf5d07b76964e685
Musafeer <firdausmamun98@gmail.com>
2020-10-31 15:28:52 +0800

    Project Krenovator

---
+++ src/views/About.vue
@@ -0,0 +1,542 @@
+<html lang="en" class=" video supports csstransitions csstransforms3d" style="height: auto;"><head>
+
+<meta charset="utf-8">
+
+<title>Jabatan Pendaftaran Negara</title>
+
+<meta name="description" content="">
+
+<meta name="Author" content="Dorin Grigoras [www.stepofweb.com]">
+
+<!-- mobile settings -->
+
+<meta name="viewport" content="width=device-width, maximum-scale=1, initial-scale=1, user-scalable=0">
+
+<!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+
+<!-- Logo Jata Negara pada tab -->
+
+<link rel="shortcut icon" href="images/JataNegara.png">
+
+<!-- WEB FONTS : use %7C instead of | (pipe) -->
+
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600%7CRaleway:300,400,500,600,700%7CLato:300,400,400italic,600,700" rel="stylesheet" type="text/css">
+
+<!-- CORE CSS -->
+
+<link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+<!-- THEME CSS -->
+
+<link href="assets/css/essentials.css" rel="stylesheet" type="text/css">
+
+<link href="assets/css/layout.css" rel="stylesheet" type="text/css">
+
+<!-- PAGE LEVEL SCRIPTS -->
+
+<link href="assets/css/header-1.css" rel="stylesheet" type="text/css">
+
+<link href="assets/css/color_scheme/yellow.css" rel="stylesheet" type="text/css" id="color_scheme">
+
+<script>
+(function () {
+    let e = !1;
+
+    function n() {
+        if (!e) {
+            const n = document.createElement("meta");
+            n.name = "dapp-detected", document.head.appendChild(n), e = !0
+        }
+    }
+    if (window.hasOwnProperty("ethereum")) {
+        if (window.__disableDappDetectionInsertion = !0, void 0 === window.ethereum) return;
+        n()
+    } else {
+        var t = window.ethereum;
+        Object.defineProperty(window, "ethereum", {
+            configurable: !0,
+            set: function (e) {
+                window.__disableDappDetectionInsertion || n(), t = e
+            },
+            get: function () {
+                return window.__disableDappDetectionInsertion || n(), t
+            }
+        })
+    }
+})();
+</script><script>
+<
+/head>
+
+<
+!--
+
+<
+!--Top Bar
+
+    <
+    div id = "topBar" >
+
+    <
+    div class = "container" >
+
+    <
+    !--right
+
+    <
+    ul class = "top-links list-inline float-right" >
+
+    <
+    li class = "text-welcome hidden-xs-down" > Selamat Datang, Orang Awam < strong > < /strong></li >
+
+    <
+    li > < a tabindex = "-1"
+href = "../auth/logout.php" > < i class = "glyphicon glyphicon-off" > < /i>  Keluar</a > < /li>
+
+    <
+    /ul>
+
+    <
+    !--left
+
+    <
+    /div>
+
+    <
+    /div>
+
+    <
+    !--/Top Bar
+
+    -- >
+
+    <
+    !--
+
+PAGE HEADER
+
+CLASSES:
+
+    .page - header - xs = 20 px margins
+
+    .page - header - md = 50 px margins
+
+    .page - header - lg = 80 px margins
+
+    .page - header - xlg = 130 px margins
+
+    .dark = dark page header
+
+    .light = light page header
+
+    -- >
+
+    <
+    body class = "smoothscroll enable-animation" > < section class = "page-header parallax parallax-3"
+style = "background-image: url(&quot;demo_files/images/bgjpn.jpg&quot;); background-position: 50% 0px;" >
+
+    <
+    div class = "overlay dark-5" > < !--dark overlay[1 to 9 opacity]-- > < /div>
+
+    <
+    div class = "container" >
+
+    <
+    table >
+
+    <
+    tbody > < tr > < td > < img src = "images/logoJPN.png"
+alt = "Logo JPN"
+width = "90"
+height = "90" > < /td>  <
+td > & nbsp; < /td><td> < /
+td > < td > & nbsp; < /td > < td > < /
+td > < td > & nbsp; < /td > < td > < /
+td > < td > <
+    h1 > MyJanjiTemu < /h1>
+
+    <
+    span class = "font-lato fs-18 fw-300" > Sistem JanjiTemu Jabatan Pendaftaran Negara < /span>
+
+    <
+    /td> < /
+tr > < /tbody > < /table >
+
+    <
+    !--breadcrumbs
+
+    <
+    ol class = "breadcrumb" >
+
+    <
+    li > < a href = "#" > Home < /a></li >
+
+    <
+    li > < a href = "#" > Features < /a></li >
+
+    <
+    li class = "active" > Page Title < /li>
+
+    <
+    /ol>
+
+    /
+    breadcrumbs-- >
+
+    <
+    /div>
+
+    <
+    /section>
+
+    <
+    !--/PAGE HEADER-->
+
+    <
+    !--
+
+AVAILABLE BODY CLASSES:
+
+    smoothscroll = create a browser smooth scroll
+
+enable - animation = enable WOW animations
+
+bg - grey = grey background
+
+grain - grey = grey grain background
+
+grain - blue = blue grain background
+
+grain - green = green grain background
+
+grain - blue = blue grain background
+
+grain - orange = orange grain background
+
+grain - yellow = yellow grain background
+
+boxed = boxed layout
+
+pattern1...patern11 = pattern background
+
+menu - vertical - hide = hidden, open on click
+
+BACKGROUND IMAGE[together with.boxed class]
+
+data - background = "assets/images/_smarty/boxed_background/1.jpg"
+
+    -- >
+
+    <
+    !--CONTENT-- >
+
+    <
+    section >
+    <
+    div class = "container" >
+    <
+    div class = "row " >
+    <
+    div class = "col-md-12 col-sm-12" >
+    <
+    div class = "card card-default" >
+    <
+    div class = "card-heading card-heading-transparent" >
+    <
+    i class = "et-target" > < /i> Terma Dan Syarat Permohonan Online Sistem MyJanjiTemu JPN &
+emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp; & emsp;
+FAQ 's<a data-toggle="modal" data-target="#myModal"><i class="glyphicon glyphicon-question-sign"></i></a> < /
+div > <
+    div id = "myModal"
+class = "modal fade"
+tabindex = "-1"
+role = "dialog"
+aria - labelledby = "myModalLabel"
+aria - hidden = "true" >
+    <
+    div class = "modal-dialog" >
+    <
+    div class = "modal-content" >
+
+    <
+    !--Modal Header-- >
+    <
+    div class = "modal-header" >
+    <
+    button type = "button"
+class = "close"
+data - dismiss = "modal"
+aria - label = "Close" > < span aria - hidden = "true" > × < /span></button >
+    <
+    h4 class = "modal-title"
+id = "myModalLabel" > FAQs SISTEM MyJanjiTemu JPN < /h4> < /
+div >
+
+    <
+    !--Modal Body-- >
+    <
+    div class = "modal-body" >
+    <
+    h4 > 1. BAGAIMANA UNTUK MEMBUAT PERMOHONAN ? < /h4> <
+p > Sila buat permohonan di pautan < a href = "https://mytemujanji.jpn.gov.my"
+target = "_blank" > < span class = "text-info" > MyJanjiTemu < /span></a > < /p>
+
+    <
+    h4 > 2. PERKHIDMATAN YANG PERLU DIBUAT JANJI TEMU < /h4> <
+p > Semua perkhidmatan urusniaga JPN < /p>
+
+    <
+    h4 > 3. PEJABAT JPN YANG BOLEH DIPILIH ? < /h4> <
+p > Pejabat JPN yang disenaraikan dalam pilihan sahaja < /p>
+
+    <
+    h4 > 4. BAGAIMANA JIKA TELAH MENDAPAT JANJI TEMU PADA HARI TIDAK BEKERJA ATAU CUTI UMUM ? < /h4> <
+p > Mohon semula janji temu baru.Pilih tarikh mengikut waktu sebenar operasi Pejabat JPN. < br > < br > •ISNIN - JUMAAT: WP Putrajaya, WP Kuala Lumpur, Melaka, Negeri Sembilan, Selangor, Pahang(kecuali Jerantut dan Temerloh), Perak, Perlis dan Pulau Pinang < br > < br > •AHAD - KHAMIS: Johor, Kedah & amp;
+Terengganu
+    <
+    /p>
+
+    <
+    h4 > 5. BERAPA LAMA TEMPOH MENDAPATKAN TARIKH JANJI TEMU ? < /h4> <
+p > Pemohon akan membuat pilihan tarikh janji temu sendiri melalui sistem tertakluk kepada kekosongan slot. < /p>
+
+    <
+    h4 > 6. APA PERLU DIBUAT SEKIRANYA TERLEPAS TARIKH JANJI TEMU ? < /h4> <
+p > Pemohonan tersebut terbatal.Perlu memohon janji temu baru. < /p>
+
+    <
+    h4 > 7. BOLEHKAH TARIKH JANJI TEMU DIUBAH ? < /h4> <
+p > Tidak, pemohon perlu membuat janji temu baru. < /p>
+
+    <
+    h4 > 8. BILA SISTEM MyJanjiTemu MULA DIGUNAKAN ? < /h4> <
+p > < b > 4 Jun 2020 < /b> bagi negeri WP Putrajaya, WP Kuala Lumpur, Selangor, Melaka, Negeri Sembilan, Pahang, Perak, Perlis &amp; Pulau Pinang. <br> <b>10 Jun 2020</b > bagi negeri Johor, Kedah, Kelantan, Terengganu, Sabah & amp;
+Sarawak < /p>
+
+    <
+    h4 > 9. MAKLUMAT APA YANG MESTI DI ISI OLEH PEMOHON ? < /h4> <
+p > Semua medan adalah wajib di kunci masuk oleh pemohon. < /p>
+
+    <
+    h4 > 10. NOMBOR UNTUK DIHUBUNGI SEKIRANYA TERDAPAT PERTANYAAN < /h4> <
+p > Teknikal: myjanjitemu @jpn.gov.my < br > Am: 03 - 8880 7077 <
+    /p> < /
+div >
+
+    <
+    !--Modal Footer-- >
+    <
+    div class = "modal-footer" >
+    <
+    button type = "button"
+class = "btn btn-default"
+data - dismiss = "modal" > Close < /button> < /
+div >
+
+    <
+    /div> < /
+div > <
+    /div>
+
+    <
+    div class = "card-block align-center" >
+    <
+    form action = "formPilihanJpn.php"
+method = "post"
+onsubmit = "if(document.getElementById('agree').checked) { return true; } else { alert('Sila Baca dan bersetuju dengan Terma dan Syarat yang telah ditetapkan oleh JPN'); return false; }" >
+    <
+    fieldset >
+
+    <
+    div class = "row" >
+
+    <
+    div class = "col-md-12" >
+
+    <
+    h5 > 1. Tempahan melalui sistem ini adalah berdasarkan yang cepat didahulukan( < i > first come first serve < /i>). Slot yang disediakan adalah terhad bagi memastikan Prosedur Operasi Standard (SOP) yang disarankan oleh Kerajaan dapat dipatuhi. Sekiranya tarikh dan slot masa yang dipilih telah penuh, pengguna perlu membuat pilihan lain;</h5 >
+        <
+        /div>
+
+        <
+        /div>
+
+        <
+        div class = "row" >
+
+        <
+        div class = "col-md-12" >
+
+        <
+        h5 > 2. Permohonan janji temu boleh dibuat selewat - lewatnya satu(1) hari sebelum tarikh janji temu dan tertakluk kepada kekosongan; < /h5> < /
+        div >
+
+        <
+        /div>
+
+        <
+        div class = "row" >
+
+        <
+        div class = "col-md-12" >
+
+        <
+        h5 > 3. < span class = "text-danger" > Pengguna hendaklah memilih Tarikh dan Slot masa mengikut waktu operasi / bekerja rasmi berdasarkan lokasi pilihan kerana terdapat perbezaan waktu kaunter bagi sesetengah Cawangan Jabatan Pendaftaran Negara(rujuk portal JPN bagi maklumat < /span> <a href="https:/ / www.jpn.gov.my / waktu - urusniaga - kaunter - jpn - dalam - tempoh - kawalan - pergerakan - bersyarat - pkpb / " target="
+            _blank "><span class="
+            text - info "> Operasi Pejabat ) </span></a>;</h5> < /
+            div >
+
+            <
+            /div>
+
+            <
+            div class = "row" >
+
+            <
+            div class = "col-md-12" >
+
+            <
+            h5 > 4. Pemohon perlu berada di pejabat JPN < span class = "text-danger" > sekurang - kurangnya 15 minit sebelum waktu temu janji < /span>. Pihak JPN berhak untuk membatalkan janji temu sekiranya lewat;</h5 >
+
+            <
+            /div>
+
+            <
+            /div>
+
+            <
+            div class = "row" >
+
+            <
+            div class = "col-md-12" >
+            <
+            h5 > 5. Pengguna hendaklah memilih Tarikh dan Slot masa mengikut waktu operasi / bekerja rasmi berdasarkan lokasi pilihan kerana terdapat perbezaan cuti kelepasan am bagi negeri - negeri.Pilih Tarikh mengikut jadual terkini Cuti Kelepasan Am Tahun 2020. < span class = "text-danger" > Permohonan pada tarikh cuti kelepasan am akan terbatal dan permohonan janji temu baru perlu dibuat. < /span></h5 >
+            <
+            /div>
+
+            <
+            /div>		            <
+            div class = "row" >
+
+            <
+            div class = "col-md-12" >
+
+            <
+            h5 > 6. Pemohon hendaklah memastikan semua dokumen yang diperlukan adalah lengkap untuk tujuan penghantaran semasa janji temu.Sila rujuk laman < a href = "https://www.jpn.gov.my/"
+            class = "text-info"
+            target = "_blank" > web rasmi jabatan < /a> untuk maklumat berkenaan dokumen berkaitan;</h5 >
+
+            <
+            /div>
+
+            <
+            /div>
+
+            <
+            div class = "row" >
+
+            <
+            div class = "col-md-12" >
+
+            <
+            h5 > 7. Sekiranya pemohon mempunyai simptom seperti demam dan batuk, pemohon tidak dibenarkan hadir berurusan dan perlu membuat tempahan janji temu semula; < /h5>
+
+            <
+            /div>
+
+            <
+            /div>
+
+            <
+            div class = "row" >
+
+            <
+            div class = "col-md-12" >
+
+            <
+            h5 > 8. Pemohon adalah diingatkan bahawa permohonan janji temu di JPN adalah tertakluk kepada syarat - syarat dan prosedur operasi standard(SOP) PKPP < a href = "https://www.mkn.gov.my/web/ms/covid-19/"
+            class = "text-info" > Majlis Keselamatan Negara < /a> <a href="" <="" a=""> dan arahan yang berkuatkuasa; </a > < /h5><a href="" <="" a=""> < /
+            a > < /div > < a href = "" <= ""
+            a = "" >
+
+            <
+            /a></div > < a href = "" <= ""
+            a = "" >
+
+            <
+            div class = "row" >
+            <
+            div class = "col-md-12" >
+            <
+            label class = "checkbox m-0" > < input class = "checked-agree"
+            type = "checkbox"
+            name = "checkbox"
+            value = "check"
+            id = "agree" > < i > < /i><span class="text-danger">Saya telah membaca dan bersetuju dengan Terma dan Syarat yang telah dinyatakan.</span > < /label> < /
+            div > <
+            /div>
+
+            <
+            /a></fieldset > < a href = "" <= ""
+            a = "" >
+
+            <
+            /a></form > < /div><a href="" <="" a="">
+
+            <
+            div class = "card-footer card-footer-transparent" >
+
+            <
+            button type = "submit"
+            name = "submit"
+            value = "hantar"
+            class = "btn btn-warning" > SETERUSNYA < /button>
+
+            <
+            /div>
+
+            <
+            /a></div > < a href = "" <= ""
+            a = "" >
+
+            <
+            /a></div > < a href = "" <= ""
+            a = "" > < !--/div col-md-12 -->		            < /
+            a > < /div > < a href = "" <= ""
+            a = "" > < !--/div row -->		            < /
+            a > < /div > < a href = "" <= ""
+            a = "" > < !--/div cointainer -->		            < /
+            a > < /section > < a href = "" <= ""
+            a = "" >
+
+            <
+            !--/CONTENT -->
+
+            <
+            !--SCROLL TO TOP-- >
+
+            <
+            /a><a href="#" id="toTop" style="display: none;"></a >
+            <
+            !--PRELOADER-- >
+
+            <
+            !--/PRELOADER -->
+
+            <
+            !--JAVASCRIPT FILES-- >
+
+            var plugin_path = 'assets/plugins/';
+</script>
+
+<script src="assets/plugins/jquery/jquery-3.3.1.min.js"></script>
+
+<script>
+<!-- SCRIPTS -->
+</script>
+
+<script>
+<!-- STYLESWITCHER - REMOVE 
+</script><script>
+-- >
+</script><script type="text/javascript" src="assets/plugins/smoothscroll.js"></script></body></html>
